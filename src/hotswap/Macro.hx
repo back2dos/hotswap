649557bd9c24507a19b2e97f2d474dcf77612cd6
Juraj Kirchheim <back2dos@googlemail.com>
2020-07-03 06:52:15 +0200

    Mark closures.

--- src/hotswap/Macro.hx
+++ src/hotswap/Macro.hx
@@ -1,15 +1,19 @@
 package hotswap;
 
-import haxe.macro.Compiler;
 #if macro
+import haxe.macro.Compiler;
 import haxe.macro.Context.*;
 import haxe.macro.Expr;
 import haxe.macro.Type;
 
+using haxe.macro.Tools;
 using tink.MacroApi;
 using StringTools;
 
 class Macro {
+
+  static final PERSIST = 'hotreload.persist';
+  static final CLOSURE = 'hotreload.closure';
 
   static function base<T:BaseType>(r:Ref<T>):BaseType
     return r.get();
@@ -35,16 +39,18 @@ class Macro {
     if (!t.isExtern) {
       t.meta.remove(':native');
 
-      var id = {
-        var a = ["hx"].concat(t.pack);
-        if (t.isPrivate)
-          a.push('$');
-        a.push(t.name);
-        macro $v{a.join('.')};
-      }
+      var id = macro $v{getId(t)};
 
       t.meta.add(':native', [id], id.pos);
     }
+
+  static function getId(t:BaseType) {
+    var a = ["hx"].concat(t.pack);
+    if (t.isPrivate)
+      a.push('$');
+    a.push(t.name);
+    return a.join('.');
+  }
 
   static function keep(o:{ meta:MetaAccess })
     if (!o.meta.has(':keep'))
@@ -60,12 +66,26 @@ class Macro {
         switch t {
           case TEnum(_.get() => e, _): move(e);
           case TInst(_.get() => c, _): move(c);
+            var statics = c.statics.get();
             for (f in c.statics.get())
               switch f.kind {
                 case FVar(_, AccNormal) | FMethod(MethDynamic):
-                  f.meta.add('hotreload.persist', [], (macro null).pos);
+                  f.meta.add(PERSIST, [], (macro null).pos);
                 default:
                   if (f.name.startsWith('onHotswap')) keep(f);
+              }
+
+            for (fields in [statics, c.fields.get()])
+              for (f in fields) {
+                function seek(t:TypedExpr) if (t != null) {
+                  t.iter(seek);
+                  switch t {
+                    case { expr: TField(_, FClosure(_, _.get() => cf))} if (!cf.meta.has(CLOSURE)):
+                      cf.meta.add(CLOSURE, [], (macro null).pos);
+                    default:
+                  }
+                }
+                seek(f.expr());
               }
 
           default:
